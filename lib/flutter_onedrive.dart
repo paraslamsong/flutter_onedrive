034ccbe71c8fa537ee2e4669eb16914a4d4b903c
timonaldenhoff <timon.aldenhoff@gmail.com>
2024-04-20 08:18:15 +0200

    Fix upload session

--- lib/flutter_onedrive.dart
+++ lib/flutter_onedrive.dart
@@ -264,13 +264,20 @@ class OneDrive with ChangeNotifier {
       var now = DateTime.now();
       var url = Uri.parse(
           "$apiEndpoint/me/drive/${_getRootFolder(isAppFolder)}:$remotePath:/createUploadSession");
-      var resp = await http.post(
-        url,
-        headers: {
-          "Authorization": "Bearer $accessToken",
-          "Content-Type": "application/json"
-        },
-      );
+      var resp = await http.post(url,
+          headers: {
+            "Authorization": "Bearer $accessToken",
+            "Content-Type": "application/json"
+          },
+          body: json.encode(
+            {
+              "@microsoft.graph.conflictBehavior": "rename",
+              "fileSystemInfo": {
+                "@odata.type": "microsoft.graph.fileSystemInfo"
+              },
+              "name": remotePath.split("/").last
+            },
+          ));
       debugPrint(
           "# Create Session: ${DateTime.now().difference(now).inMilliseconds} ms");
 
